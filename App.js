3aa03a8020c15461b320b754b79016fb752ea616
davidblee07 <93348233+davidblee07@users.noreply.github.com>
2022-12-22 01:18:52 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import DrawerNavigation from "./navigation/DrawerNavigation";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <DrawerNavigation />
+    </NavigationContainer>
+  );
+}
---
+++ navigation/TabNavigator.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import CreatePost from '../screens/CreatePost';
+import Feed from '../screens/Feed';
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator= ()=> {
+ return(
+    <Tab.Navigator
+     screenOptions={({route}) => ({
+      tabBarIcon:({focuzed, color, size})=> {
+        let iconName;
+        if(route.Name === 'Feed'){
+          iconName = focuzed ? 'book' : 'book-outline';
+        }else if(route.Name === 'Profile'){
+            iconName = focuzed ? 'create' : 'create-outline';  
+        }
+        return <IonIcons name={iconName} size={size} color={color} />    
+     }
+  })}
+       tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor:'gray'
+       }}
+    >
+        <Drawer.Screen name='Criar Postagem' component={CreatePost}/>  
+       <Drawer.Screen name='Feed' component={Feed}/>
+    </Tab.Navigator>
+ )
+}
+
+export default BottomTabNavigator;
+
